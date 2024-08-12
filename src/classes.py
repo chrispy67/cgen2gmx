9d045a9518394d427513115e71941d66f1a052d0
Christian <109503264+chrispy67@users.noreply.github.com>
2024-08-12 14:01:05 -0700

    reorganized modules

---
+++ src/classes.py
@@ -0,0 +1,267 @@
+import logging as log
+import pandas as pd
+import re
+import os
+import config
+
+
+# import user flags stored in config file
+input_file_CHARMM = config.input_file_CHARMM
+input_file_CGEN = config.input_file_CGEN
+output_file = config.output_file
+unit_convert = config.convert_to_kj
+
+
+class MolecularData:
+    def __init__(self):
+        
+        #broken down by file type for future functionality
+        self.data = {
+            'merged.rtp': {
+                'atom': [],
+                'connectivity': []
+            },
+            'ffbonded.itp': {
+                'bonds': [],
+                'angles': [],
+                'dihedrals': [],
+                'impropers': []
+            }
+        }
+
+#store data in the class and empty arrays above
+    def add_atom(self, atom_data):
+        self.data['merged.rtp']['atom'].append(atom_data)
+
+    def add_connectivity(self, connectivity_data):
+        self.data['merged.rtp']['connectivity'].append(connectivity_data)
+
+    def add_bonds(self, bond_data):
+        self.data['ffbonded.itp']['bonds'].append(bond_data)
+    def add_angles(self, angle_data):
+        self.data['ffbonded.itp']['angles'].append(angle_data)
+
+    def add_dihedrals(self, dihedral_data):
+        self.data['ffbonded.itp']['dihedrals'].append(dihedral_data)
+
+    def add_impropers(self, improper_data):
+        self.data['ffbonded.itp']['impropers'].append(improper_data)
+
+#retrieve data from the class
+
+    def get_atoms(self):
+        #not used yet; the issue of unique entries is not present in merged.rtp
+        return pd.DataFrame(self.data['merged.rtp']['atom'])
+
+    def get_connectivity(self):
+        #not used yet; the issue of unique entries is not present in merged.rtp
+        return pd.DataFrame(self.data['merged.rtp']['connectivity'])
+
+    def get_bonds(self):
+        df = pd.DataFrame(self.data['ffbonded.itp']['bonds'])
+        df['b0'] = pd.to_numeric(df['b0'], errors='coerce')
+        df['kb'] = pd.to_numeric(df['kb'], errors='coerce')
+
+        #CGEN uses kcal by default, but GROAMCS uses kJ
+        if unit_convert:
+            b0_factor = 0.1 #Å -> nm
+            kb_factor = 836.8 #kcal/molÅ^-2 -> kJ/mol*nm^-2 #CORRECT FOR FUNC_1
+            df['b0'] *= b0_factor
+            df['kb'] *= kb_factor
+
+            unit_dict = {
+                'b0': 'nm',
+                'kb': 'kJ/mol*nm^-2'}
+        
+        else:
+            unit_dict = {
+                'b0': 'Å',
+                'kb': 'kcal/mol*Å^2'
+            }
+
+        # Rename the unit columns to distinguish from the original columns
+        units_df = pd.DataFrame({key: [value] for key, value in unit_dict.items()}) # (1, 4)
+        units_df = pd.concat([units_df] * len(df), ignore_index=True) # (len(parsed_entries), 4) each column is the same value
+        units_df = units_df.add_suffix('_unit')
+        
+        df_with_units = pd.concat([df, units_df], axis=1)# (len(parsed_entries), 11)
+
+        # df containing all entries from cgen file with units in each row of *_unit keys
+        return df_with_units
+
+#angles: ;      i        j        k  func       theta0       ktheta      ub0       kub
+
+    def get_angles(self):
+        df = pd.DataFrame(self.data['ffbonded.itp']['angles'])
+        df[['ktheta', 'theta0', 'kub', 'ub0']] = df[['ktheta', 'theta0', 'kub', 'ub0']].apply(pd.to_numeric, errors='coerce')
+
+        #CGEN uses kcal by default, but GROAMCS uses kJ
+        if unit_convert:
+            theta_factor = 1 #deg -> deg
+            ktheta_factor = 8.368 #kcal -> kJ * func_5_factor
+            ub0_factor = 0.1 #Å -> nm
+            kub_factor = 836.8 #kcal/molÅ^-2 -> kJ/mol*nm^-2 #CORRECT FOR FUNC_5
+            df['theta0'] *= theta_factor #for redundancy, units are same (deg)
+            df['ktheta'] *= ktheta_factor
+            df['kub'] *= kub_factor
+            df['ub0'] *= ub0_factor
+
+            #UNITS
+            unit_dict = {
+                'ktheta': 'kJ/mol',
+                'theta0': 'deg',
+                'kub': 'kJ/mol*nm^-2',
+                'ub0': 'nm'} 
+        else:
+            #UNITS
+            unit_dict = {
+                'ktheta': 'kcal/mol',
+                'theta0': 'deg',
+                'kub': 'kcal/mol*Å^-2',
+                'ub0': 'Å'} 
+
+        # Rename the unit columns to distinguish from the original columns
+        units_df = pd.DataFrame({key: [value] for key, value in unit_dict.items()}) # (1, 4)
+        units_df = pd.concat([units_df] * len(df), ignore_index=True) # (len(parsed_entries), 4) each column is the same value
+        units_df = units_df.add_suffix('_unit')
+        df_with_units = pd.concat([df, units_df], axis=1)# (len(parsed_entries), 11)
+
+        # df containing all entries from cgen file with units in each row of *_unit keys
+        return df_with_units
+
+    def get_dihedrals(self):
+        df = pd.DataFrame(self.data['ffbonded.itp']['dihedrals'])
+        df[['kphi', 'phi0', ]] = df[['kphi', 'phi0']].apply(pd.to_numeric, errors='coerce')
+
+        #CGEN uses kcal by default, but GROAMCS uses kJ
+        if unit_convert:
+            phi_0_factor = 1 #deg -> deg
+            kphi_factor = 4.184 #kcal -> kJ
+
+            df['kphi'] *= kphi_factor #TO BE MULTIPLIED BY MULTI FOR THIS FUNCTION
+            df['phi0'] *= phi_0_factor
+
+            unit_dict = {
+                'kphi': 'kJ/mol',
+                'phi0': 'deg'}
+        else:
+            unit_dict = {
+                'kphi': 'kcal/mol',
+                'phi0': 'deg'}
+
+        # Rename the unit columns to distinguish from the original columns
+        units_df = pd.DataFrame({key: [value] for key, value in unit_dict.items()}) # (1, 4)
+        units_df = units_df.add_suffix('_unit')
+        units_df = pd.concat([units_df] * len(df), ignore_index=True) # (len(parsed_entries), 4) each column is the same value
+        
+        df_with_units = pd.concat([df, units_df], axis=1)# (len(parsed_entries), 11)
+
+        # df containing all entries from cgen file with units in each row of *_unit keys
+        return df_with_units
+
+
+    def get_impropers(self):
+        df = pd.DataFrame(self.data['ffbonded.itp']['impropers'])
+        
+        try: #often empty
+            df[['kphi', 'phi0']] = df[['kphi', 'phi0']].apply(pd.to_numeric, errors='coerce')
+
+        except KeyError as e:
+            print(f'Key Error! {e}', 'is the parameter field entry?')
+        
+        #CGEN uses kcal by default, but GROAMCS uses kJ
+        if unit_convert:
+            keta_factor = 8.368 #kcal/mol -> kJ/mol *2
+            eta0_factor = 1 #deg -> deg 
+            df['kphi'] *= keta_factor
+            df['phi0'] *= eta0_factor
+
+            unit_dict = {
+                'kphi': 'kJ/mol',
+                'phi0': 'deg'}
+        else:
+            unit_dict = {
+                'kphi': 'kcal/mol',
+                'phi0': 'deg'}
+
+        units_df = pd.DataFrame({key: [value] for key, value in unit_dict.items()}) # (1, 2)
+        units_df = pd.concat([units_df] * len(df), ignore_index=True) # (len(parsed_entries), 2) each column is the same value
+        units_df = units_df.add_suffix('_unit')        
+
+        df_with_units = pd.concat([df, units_df], axis=1)# (len(parsed_entries), 11)
+
+        # df containing all entries from cgen file with units in each row of *_unit keys
+        return df_with_units
+
+    #HARDCODED headers that are standard for modern CHARMM forcefields
+    def bond_header(self):
+        header = '[ bondtypes ]' +'\n'+'; i        j          func      b0[b0_unit]         kb[kb_unit]'
+        return str(header)
+
+    def angle_header(self):
+        header = '[ angletypes ]' + '\n'+'; i        j        k           func  theta0[theta0_unit]  ktheta[ktheta_unit]  ub0[ub0_unit]    kub[kub_unit]'
+        return str(header)
+
+    def dihedral_header(self):
+        header = '[ dihedraltypes ]'+'\n'+'; i       j       k       l         func   phi0[phi0_unit]   kphi[kphi_unit] mult'
+        return str(header)
+
+    def improper_header(self):
+        header = '[ dihedraltypes ] ;IMPROPERS'+'\n'+'; i       j       k       l         func   phi0[phi0_unit]   kphi[kphi_unit]'
+        return(str(header))
+
+
+#nearly identical class to MolecularData, but less functionality
+
+class ForceFieldInfo:
+
+    def __init__(self):
+        self.data = {
+            'merged.rtp': {
+                'atom': [],
+                'connectivity': []
+            },
+            'ffbonded.itp': {
+                'bonds': [],
+                'angles': [],
+                'dihedrals': [],
+                'impropers': []
+            }
+        }
+
+    def add_atom(self, atom_data):
+        self.data['merged.rtp']['atom'].append(atom_data)
+
+    def add_connectivity(self, connectivity_data):
+        self.data['merged.rtp']['connectivity'].append(connectivity_data)
+
+    def add_bonds(self, bond_data):
+        self.data['ffbonded.itp']['bonds'].append(bond_data)
+
+    def add_angles(self, angle_data):
+        self.data['ffbonded.itp']['angles'].append(angle_data)
+
+    def add_dihedrals(self, dihedral_data):
+        self.data['ffbonded.itp']['dihedrals'].append(dihedral_data)
+
+    def add_impropers(self, improper_data):
+        self.data['ffbonded.itp']['impropers'].append(improper_data)
+
+    def get_atoms(self):
+        return pd.DataFrame(self.data['merged.rtp']['atom'])
+
+    def get_connectivity(self):
+        return pd.DataFrame(self.data['merged.rtp']['connectivity'])
+
+    def get_bonds(self):
+        return pd.DataFrame(self.data['ffbonded.itp']['bonds'])
+
+    def get_angles(self):
+        return pd.DataFrame(self.data['ffbonded.itp']['angles'])
+
+    def get_dihedrals(self):
+        return pd.DataFrame(self.data['ffbonded.itp']['dihedrals'])
+
+    def get_impropers(self):
+        return pd.DataFrame(self.data['ffbonded.itp']['impropers'])
+
---
+++ src/parse_files.py
@@ -0,0 +1,215 @@
+import logging as log
+from classes import MolecularData, ForceFieldInfo
+
+def parse_cgen(file_path):
+    molecular_data = MolecularData()
+    
+    #selection indicates where to store data in ForceFieldInfo()
+    current_selection = None
+
+    #CGEN has some lines to skip that aren't parameters OR comments...
+    #might need to add things to this list if you are having issues with reading files. 
+    to_skip = ('*', '#', '!', ';', 'END ', 'RETURN', ' ', 'RESI')
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if not line or line.startswith(to_skip):
+                continue
+            elif not line.strip():
+                current_selection = None
+
+            ####----merged.itp----####
+            if line.startswith('ATOM'):
+                current_selection = 'ATOM'
+            elif line.startswith('BOND '): #SINGULAR-merged.rtp
+                current_selection = 'BOND'  #SINGULAR-merged.rtp
+            elif line.startswith('IMPR '):
+                current_selection = 'IMPR'
+            
+            ####----ffbonded.itp----####
+            elif line.startswith('BONDS'): #PLURAL-ffbonded.itp
+                current_selection = 'BONDS'  #PLURAL-ffbonded.itp
+            elif line.startswith('ANGLES'):
+                current_selection = 'ANGLES'
+            elif line.startswith('DIHEDRALS'):
+                current_selection = 'DIHEDRAL'
+            elif line.startswith('IMPROPERS'):
+                current_selection = 'IMPROPER'
+            # Add more sections as needed
+
+            #FOR SELECTIONS
+            
+            if current_selection == 'ATOM':
+                atom_data = line.split()
+                row = {
+                    'i': atom_data[1],
+                    'j': atom_data[2],
+                    'charge': atom_data[3]
+                }
+                molecular_data.add_atom(row)
+
+
+#HANDLES THE PLURAL AND SINGLUAR VERSIONS THAT CgenFF likes to use.
+#similar treatment to 
+            elif current_selection == 'BOND ' and len(line.split()) != 1: #avoids header
+                bond_data = line.split()
+                row = {
+                'i': bond_data[1],
+                'j': bond_data[2]
+                }
+                molecular_data.add_connectivity(row)
+            elif current_selection == 'BONDS' and len(line.split()) != 1:
+                connectivity_data = line.split()
+                row = {
+                'i': connectivity_data[0],
+                'j': connectivity_data[1],
+                'func': int(1),
+                'kb': connectivity_data[2], ###UNITS spring constant
+                'b0': connectivity_data[3]  ###UNITS A -> nm
+                }
+                molecular_data.add_bonds(row)
+
+
+
+#angles: ;      i        j        k  func       theta0       ktheta      ub0       kub
+
+            elif current_selection == 'ANGLES' and len(line.split()) != 1 :
+                angle_data = line.split()
+                # Only some entries have Urey-Bradley potentials. Otherwise, the entry for
+                #   these columns is zero.
+                if angle_data[6] == '!':
+                    row = {
+                        'i': angle_data[0],
+                        'j': angle_data[1],
+                        'k': angle_data[2],
+                        'func': 5,
+                        'ktheta': angle_data[3],
+                        'theta0': angle_data[4],
+                        'kub': float(0.00),
+                        'ub0': float(0.00)
+                    }
+                else:
+                        row = {
+                        'i': angle_data[0],
+                        'j': angle_data[1],
+                        'k': angle_data[2],
+                        'func': 5, 
+                        'ktheta': angle_data[3],
+                        'theta0': angle_data[4],
+                        'kub': angle_data[5],
+                        'ub0': angle_data[6]
+                    }
+                molecular_data.add_angles(row)
+            
+            elif current_selection == 'DIHEDRAL' and len(line.split()) != 1:
+                dihedral_data = line.split()
+                row = {
+                    'i': dihedral_data[0],  #STANDARD DIHEDRALS HAVE FUNCTION 9
+                    'j': dihedral_data[1],
+                    'k': dihedral_data[2],
+                    'l': dihedral_data[3],
+                    'func': int(9),
+                    'kphi': dihedral_data[4],
+                    'multi': dihedral_data[5],
+                    'phi0': dihedral_data[6]
+                }
+                molecular_data.add_dihedrals(row)
+
+
+# ;   i       j       k       l       func    phi0 [deg]    kphi [kJ/mol]          mult
+
+
+            elif current_selection == 'IMPROPER' and len(line.split()) != 1:
+                
+                improper_data = line.split()
+                row = {
+                    'i': improper_data[0],    #IMPROPERS HAVE FUNCTION 2
+                    'j': improper_data[1],
+                    'k': improper_data[2],
+                    'l': improper_data[3],
+                    'kphi': improper_data[4],
+                    #'multi': improper_data[5], # there are NO multiplicity functions here
+                    'func': str(2),
+                    'phi0': improper_data[6]
+                }
+                
+                molecular_data.add_impropers(row)
+
+    return molecular_data
+
+def parse_ff(ff_file): ##WIP
+    selection = False
+    ff_data = ForceFieldInfo()
+
+    with open(ff_file, 'r') as file:
+        for line in file:
+            try:
+                data = line.split()
+            except IndexError:
+                continue
+
+            # Checks for comments
+            if not line.strip() or line.startswith(('*', '#', ';', '!')):
+                continue
+            if line.startswith('[ '):
+                selection = line.split('[')[-1].split(']')[0].strip().lower()
+                continue
+
+            # Adheres to standard CHARMM forcefield headers, but subject to change.
+            if selection == 'bondtypes':
+                try:
+                    bond_data = line.split()
+                    existing_data = {
+                        'i': bond_data[0],
+                        'j': bond_data[1]
+                    }
+                    ff_data.add_bonds(existing_data)
+                except IndexError:
+                    continue
+            
+            # Adheres to standard CHARMM forcefield headers, but subject to change.
+            elif selection == 'angletypes':
+                try:
+                    angle_data = line.split()
+                    existing_data = {
+                        'i': angle_data[0],
+                        'j': angle_data[1],
+                        'k': angle_data[2]
+                    }
+                    ff_data.add_angles(existing_data)
+                except IndexError:
+                    continue
+
+            # Adheres to standard CHARMM forcefield headers, but subject to change.
+            # 'func' being 9 is the only thing that makes this an improper.  
+            elif selection == 'dihedraltypes' and data[4] == '9': #ACTUAL DIHEDRALS
+                try:
+                    dihedral_data = line.split()
+                    existing_data = {
+                        'i': dihedral_data[0],
+                        'j': dihedral_data[1],
+                        'k': dihedral_data[2],
+                        'l': dihedral_data[3],
+                        'multi': dihedral_data[7] 
+                    }
+                    ff_data.add_dihedrals(existing_data)
+                except IndexError:
+                    continue
+
+            # Adheres to standard CHARMM forcefield headers, but subject to change.
+            elif selection == 'dihedraltypes' and data[4] == '2': #IMPROPERS
+                try:
+                    improper_data = line.split()
+                    existing_data = {
+                        'i': improper_data[0],
+                        'j': improper_data[1],
+                        'k': improper_data[2],
+                        'l': improper_data[3],
+                        'func':improper_data[4]
+                    }
+                    ff_data.add_impropers(existing_data)
+                except IndexError:
+                    continue
+
+    return ff_data
