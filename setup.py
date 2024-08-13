d91a777bea9f050032240005291c29581182e2bb
Christian <109503264+chrispy67@users.noreply.github.com>
2024-08-13 14:30:12 -0700

    tuning Pypi setup

---
+++ setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup, find_namespace_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="nomelt",  
+    version="2023.1",  # Required
+    long_description=long_description, 
+    long_description_content_type="text/markdown", 
+    url="https://github.com/chrispy67/cgen2gmx",
+    author="Christian Phillips ", 
+    author_email="christian_phillips1@msn.com",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="molecular dynamics, charmm, forcefield, computational chemistry",
+    packages=find_namespace_packages(),
+
+    #python_requires=">=3.7, <4",
+    #extras_require={ 
+    #    "test": ["coverage", 'pytest'],
+    #},
+
+    #package_data={},
+    #data_files=None, 
+    #entry_points={},
+)
