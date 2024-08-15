0f7a8432da0e9c9a9bf980f1c58723b699c4e58b
Christian <109503264+chrispy67@users.noreply.github.com>
2024-08-15 15:34:16 -0700

    testing pypi publish + setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="nomelt",  
-    version="2023.1",  # Required
+    # name="nomelt",  
+    version="1",  # Required
     long_description=long_description, 
     long_description_content_type="text/markdown", 
     url="https://github.com/chrispy67/cgen2gmx",
