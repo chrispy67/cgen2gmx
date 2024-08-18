fd65b14aedcf5f30fcea24edf608f96c7fe9b832
Christian <109503264+chrispy67@users.noreply.github.com>
2024-08-18 15:01:31 -0700

    edited setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,46 @@
 
 from setuptools import setup, find_namespace_packages
-import pathlib
 
-here = pathlib.Path(__file__).parent.resolve()
+# Load the long description from the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-# Get the long description from the README file
-long_description = (here / "README.md").read_text(encoding="utf-8")
+setup(
+    name="cgen2gmx",  
+    version="1.0",  
+    description="A small commandline tool for managing forcefield parameters used in molecular dynamics simulations", 
+    long_description=long_description,  # Detailed description from README
+    long_description_content_type="text/markdown",  # Type of content in long description
+    url="https://github.com/chrispy67/cgen2gmx",  
+    author="Christian Phillips ", 
+    author_email="christian_phillips1@msn.com", 
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="molecular dynamics, charmm, forcefield, computational chemistry",  # Relevant keywords
+    packages=find_namespace_packages(),  # Finds all packages under the namespace
+    python_requires=">=3.7",  
+    install_requires=[
+        # List your package dependencies here, e.g.,
+        # 'pandas', 'os', 'click'
+    ],
+    entry_points={
+        'console_scripts': [
+            'cgen2gmx=cgen2gmx:main', 
+        ],
+    },
+    include_package_data=True,  # Includes additional files specified in MANIFEST.in
+    project_urls={  # Optional additional URLs
+        "Bug Reports": "https://github.com/chrispy67/cgen2gmx/issues",
+        "Source": "https://github.com/chrispy67/cgen2gmx",
+    },
+)
+
+
+
+
 
 setup(
     name="cgen2gmx",  
